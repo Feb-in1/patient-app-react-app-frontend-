893bc121aa13ece85cce177608073c2de168fba1
Feb-in1 <febinfrancisk10@gmail.com>
2024-06-13 02:13:20 +0530

    View All completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddPatient from './components/AddPatient';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
     <div>
       <BrowserRouter><Routes>
         <Route path='/' element={<AddPatient/>}/>
+        <Route path='/ViewAll' element={<ViewAll/>}/>
       </Routes></BrowserRouter>
       
     </div>
